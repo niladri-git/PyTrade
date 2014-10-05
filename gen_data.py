2700f7d3e7ff05ae30cab15d10be59135a8137f4
Niladri Datta <dattaniladri@gmail.com>
2014-10-06 00:06:15 +0530

    Added Calls and Puts

--- gen_data.py
+++ gen_data.py
@@ -15,7 +15,8 @@ with open("cont.txt", "r") as file:
 for key in cont_dict.keys():
 	print(key, "=>", cont_dict[key])
 	
-close = 8000
+close = 7800
+print("\nclose: ", close)
 
 def exp_calls():
 	print("\nExpiring Calls\n")
@@ -51,10 +52,32 @@ def exp_calls():
 
 def exp_puts():
 	print("\nExpiring Puts")
+	
+	for key in cont_dict.keys():
+		if "PE" in key:
+		
+			' '.join(key.split())	
+			(strike, type) = key.split()
+			qty = cont_dict[key]
+			
+			deno = int(strike)
+			con_val = deno * qty
+			cls_val = close * -qty
+			dif_val = con_val + cls_val
+			
+			#print(dif_val)
+			
+			if (close > deno):
+				net_val = 0
+			else:
+				net_val = dif_val;
+				
+			print(strike, "=>", qty, "\t", "Exp Val:", net_val)
+
 
 def exp_futs():
 	print("\nExpiring Futures")
   
 exp_calls()
-#exp_puts()
+exp_puts()
 #exp_futs()
